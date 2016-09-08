9d8463cdf0768f1079e3f07553d77c001231b9d0
ashersz <asher@curiyo.com>
2016-09-08 16:46:29 +0300

    minimal working solution

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -4,9 +4,21 @@
   angular.module('LunchCheck', [])
 
   .controller('LunchCheckController', LunchCheckController)
-
   LunchCheckController.$inject = ['$scope']
   function LunchCheckController ($scope) {
-
+    $scope.lunch_menu = ''
+    $scope.message = ''
+    $scope.check_menu = function () {
+      var arr = $scope.lunch_menu.split(',')
+      if (arr.length > 3) {
+        $scope.message = 'Too much!'
+      } else {
+        if ($scope.message.length > 0) {
+          $scope.message = 'Enjoy!'
+        } else {
+          $scope.message = 'Please enter data first'
+        }
+      }
+    }
   }
 })()
