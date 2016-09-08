23ac03a5fd175878c1facbc02ddda7cc56c429e4
ashersz <asher@curiyo.com>
2016-09-08 17:34:30 +0300

    did the bonus with ng-class

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -11,11 +11,14 @@
     $scope.check_menu = function () {
       var arr = $scope.lunch_menu.split(',')
       if (arr.length > 3) {
+        $scope.color = 'green'
         $scope.message = 'Too much!'
       } else {
         if ($scope.lunch_menu.length > 0) {
+          $scope.color = 'green'
           $scope.message = 'Enjoy!'
         } else {
+          $scope.color = 'red'
           $scope.message = 'Please enter data first'
         }
       }
