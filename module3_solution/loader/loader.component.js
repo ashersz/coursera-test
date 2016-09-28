d8d3bd533ab4d9af7bbc5d1d8f982783b5b5c70a
ashersz <asher@curiyo.com>
2016-09-28 13:24:49 +0300

    added loader implementation

--- module3_solution/loader/loader.component.js
+++ module3_solution/loader/loader.component.js
@@ -3,6 +3,26 @@
 
   angular.module('Loader')
   .component('itemsLoaderIndicator', {
-    templateUrl: 'loader/itemsloaderindicator.template.html'
+    templateUrl: 'loader/itemsloaderindicator.template.html',
+    controller: LoaderController
   })
+  LoaderController.$inject = ['$rootScope', '$element']
+  function LoaderController ($rootScope, $element) {
+    var $ctrl = this
+
+    var cancelListener = $rootScope.$on('foundItems:processing', function (event, data) {
+      console.log('Event: ', event)
+      console.log('Data: ', data)
+
+      if (data.on) {
+        $element.find('div').css('display', 'block')
+      } else {
+        $element.find('div').css('display', 'none')
+      }
+    })
+
+    $ctrl.$onDestroy = function () {
+      cancelListener()
+    }
+  }
 })()
--- module3_solution/narrowsearch/narrow.controller.js
+++ module3_solution/narrowsearch/narrow.controller.js
@@ -4,16 +4,21 @@
   angular.module('NarrowItDownApp')
   .controller('NarrowItDownController', NarrowItDownController)
 
-  NarrowItDownController.$inject = ['MenuSearchService']
-  function NarrowItDownController (MenuSearchService) {
+  NarrowItDownController.$inject = ['MenuSearchService', '$rootScope']
+  function NarrowItDownController (MenuSearchService, $rootScope) {
     var ctrl = this
     ctrl.narrowSearch = function () {
+      ctrl.found = []
+      $rootScope.$broadcast('foundItems:processing', { on: true })
       var promise = MenuSearchService.getMatchedMenuItems(ctrl.searchTerm)
       promise.then(function (response) {
         ctrl.found = response
       })
       .catch(function (error) {
         console.log('error,' + error.message)
+      })
+      .finally(function () {
+        $rootScope.$broadcast('foundItems:processing', { on: false })
       })
     }
     ctrl.removeFound = function (index) {
