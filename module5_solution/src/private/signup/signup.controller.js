ff99de6d8faa583535d691942cc333bc3847846c
ashersz <asher@curiyo.com>
2016-10-21 10:46:53 +0300

    added MenuService.getMenuItem

--- module5_solution/src/private/signup/signup.controller.js
+++ module5_solution/src/private/signup/signup.controller.js
@@ -4,12 +4,13 @@
   angular.module('private')
   .controller('SignupController', SignupController)
 
-  // SignupController.$inject = ['menuItems']
-  function SignupController () {
+  SignupController.$inject = ['MenuService']
+  function SignupController (MenuService) {
     var reg = this
 
     reg.submit = function () {
       reg.completed = true
+      MenuService.getMenuItem(reg.user.menu_item)
     }
   }
 })()
