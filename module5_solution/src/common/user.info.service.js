1c90f2e6533e8da1d90c6aa30898aa31e25b8e25
ashersz <asher@curiyo.com>
2016-10-25 14:37:46 +0300

    saving the user data

---
+++ module5_solution/src/common/user.info.service.js
@@ -0,0 +1,23 @@
+(function () {
+  'use strict'
+  angular.module('common')
+  .service('UserInfoService', UserInfoService)
+  UserInfoService.$inject = ['$rootScope', '$window']
+  function UserInfoService ($rootScope, $window) {
+    var service = this
+    service.userInfo = {
+      lastname: '',
+      firstname: '',
+      email: '',
+      menu_item: ''
+    }
+    service.setUserInfo = function (userInfo) {
+      $window.sessionStorage.UserInfoService = angular.toJson(userInfo)
+    }
+    service.getUserInfo = function () {
+      $window.service.UserInfoService = angular.fromJson($window.sessionStorage.UserInfoService)
+    }
+    $rootScope.$on('saveUserInfo', service.setUserInfo)
+    $rootScope.$on('getUserInfo', service.getUserInfo)
+  }
+})()
--- module5_solution/src/private/signup/signup.controller.js
+++ module5_solution/src/private/signup/signup.controller.js
@@ -4,14 +4,15 @@
   angular.module('private')
   .controller('SignupController', SignupController)
 
-  SignupController.$inject = ['MenuService']
-  function SignupController (MenuService) {
+  SignupController.$inject = ['MenuService', 'UserInfoService']
+  function SignupController (MenuService, UserInfoService) {
     var reg = this
 
     reg.submit = function () {
       reg.completed = true
       MenuService.getMenuItem(reg.user.menu_item).then(function (response) {
         reg.info = 'Your information has been saved'
+        UserInfoService.setUserInfo(reg.user)
         return response
       }, function (error) {
         console.log('error= ' + error.statusText)
